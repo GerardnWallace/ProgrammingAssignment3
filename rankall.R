eb2153d0c0d6d99a83d26b06f9f931d57f5587e1
Gerard Wallace <GerardNWallace@gmail.com>
2017-02-05 22:29:44 -0600

    rankall finished

--- rankall.R
+++ rankall.R
@@ -1,7 +1,51 @@
+# The function rankal retreives the hospital with the given 30-day 
+# mortality rank (lowest to highest) for each state
+#         Args 
+#           outcome: A character string specifying "heart failure", "heart 
+#           attack" or "pneumonia".
+#           
+#           num: A number specifying the desired rank, "best" and "worst" are
+#           are accepted values
+#
+#           Returns
+#           A dataframe with the name of the hopsital with the given
+#           30-day mortality for the desired state.  Ties in 30-day mortality 
+#           are broken by alphabetical sorting of hospital names.
+
 rankall <- function(outcome, num = "best") {
-        ## Read outcome data
-        ## Check that state and outcome are valid
-        ## For each state, find the hospital of the given rank
-        ## Return a data frame with the hospital names and the
-        ## (abbreviated) state name
+        data <- read.csv("outcome-of-care-measures.csv",
+                         na.strings = "Not Available",
+                         stringsAsFactors = FALSE)
+        
+        #Checking Arguement Validity
+        valid.outcomes <-c("heart failure", "heart attack", "pneumonia")
+        if(!any(outcome == valid.outcomes)) {
+                stop("invalid outcome")
+        }
+        
+        rank.list <- by(data, data[,7], function(x) {
+                
+        #Setting an index to specify desired outcome 30-day mortality
+                switch(outcome,
+                       "heart failure" = {col_index <- 17},
+                       "heart attack" = {col_index <- 11},
+                       "pneumonia" = {col_index <- 23})
+                
+        #Order by hopsitals by outcome and name, ommitting NAs
+                order_index <- order(x[,col_index], x[,2], na.last = NA)        
+                hospitals <- x[order_index, c(2, 7)]
+                names(hospitals) <- c("hospital", "state")
+                
+        #Setting num for best and worst cases
+                if (num == "best") {
+                        num <- 1
+                }
+                if (num == "worst") {
+                        num <- length(hospitals)
+                }
+        
+        #Return Best hospital by outcome
+                hospitals[num, ]
+        })
+        do.call(rbind, rank.list)
 }
